9740641a311fbe79a18ef92bf56aa3328c82c65a
manas-sde <mishramanas481@gmail.com>
2025-01-20 00:42:52 +0530

    support for remote mongo

--- projectFolder/settings.py
+++ projectFolder/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'djongo',
         'NAME': 'mishipayDjangoDB',
         'CLIENT': {
-                'host': 'mongodb://127.0.0.1:27017/'
+                'host': os.environ.get('MONGO_HOST','mongodb://127.0.0.1:27017/')
         }
     }
 }
